2b99e2b3cda7ec2cd4e4d10ef51760e81d61d0f0
Shiva kumar <24bk1a6675@stpetershyd.com>
2026-08-11 18:53:24 +0530

    execution flow

---
+++ Basics/Basic/c.c
@@ -0,0 +1,28 @@
+1) we write the c code in the highlevel programing language 
+2) we will be compiling the c program by tcc.exe tcc is turbo c compiler is their is any error then it throught syntax error
+3) then we get an object file .obj which is stored in the harddisk
+4) then linker will link the obj file and library functions by static 
+5) then it will generate a executable file.exe which is again stored in the hard disk it is os dependent
+6) loader will load the files in to the os 
+7) then it will be loaded into the ram 
+8) then it is loadded into cpu
+9) then we will be getting the output 
+
+
+10)C Source Code (.c)
+       ↓
+    Compiler
+       ↓
+Object File (.obj / .o)
+       ↓
+     Linker
+       ↓
+Executable File (.exe)
+       ↓
+     Loader
+       ↓
+      RAM
+       ↓
+      CPU
+       ↓
+    Output
