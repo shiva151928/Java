29f21d2073cad9974661e1b44589e200dc97e411
Shiva kumar <24bk1a6675@stpetershyd.com>
2026-08-11 19:07:35 +0530

    Create structure.java

---
+++ Basics/Basic/structure.java
@@ -0,0 +1,21 @@
+1) we will be writing our program in the notepad
+
+  STRUCTURE OF JAVA PROGRAM :-
+
+ Class <name>
+  {
+    Public Static void Main(String arg[])
+      {
+        
+      }
+  }
+
+class - class is a collection of data members and member functions of a program
+public
+private  both are Access Specifier's
+
+public - members declared as public can be accessed from outside the class and within the class 
+private - members declared as private can be accessed with in the class only
+static - members declared as static can be accessed using class name
+void - return type 
+main - entry point of a program
