f6c2429243155f92936a6468aa320bcf631b3616
Shiva kumar <24bk1a6675@stpetershyd.com>
2026-08-11 18:58:26 +0530

    execution flow

---
+++ Basics/Basic/java.java
@@ -0,0 +1,31 @@
+1) we write the java code in the high level programing languague its extension is .java
+2) then compiler will compile the source code if any error give error
+3) then .class file will be generated that is not but byte code will be generated it is os independent
+4) then we have jvm and jit this are os dependent
+5) here the linking will be dynamic it will take care of jit 
+6) then it is loaded into os 
+7) then ram 
+8) then cpu 
+9) then output
+
+10)
+Java Source Code (.java)
+          ↓
+       Compiler
+       javac
+          ↓
+     Bytecode (.class)
+          ↓
+         JVM
+          ↓
+   Class Loader
+          ↓
+    Bytecode Verification
+          ↓
+         JIT
+          ↓
+   Native Machine Code
+          ↓
+         CPU
+          ↓
+        Output
