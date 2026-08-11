cd848bae3402e4ef102e97d1d53523dc1777c423
Shiva kumar <24bk1a6675@stpetershyd.com>
2026-08-11 19:10:33 +0530

    Create prg1.java

---
+++ Basics/Programs/prg1.java
@@ -0,0 +1,17 @@
+class abc
+{
+  public static void main(String args[])
+  {
+      System.Out.println("Hello");
+  }
+}
+
+output : Hello
+
+  printf- function
+
+  System.out.println()
+
+  System=class
+  out=object
+  println=method/ function
