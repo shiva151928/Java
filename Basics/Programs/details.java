1ba1f73b09b8aec89d7475c7d9ae81234c8c2cf8
Shiva kumar <24bk1a6675@stpetershyd.com>
2026-08-12 09:59:05 +0530

    deatils of a person

---
+++ Basics/Programs/details.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+class Main {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        String name,age,branch,clg;
+        name=sc.nextLine();
+        age = sc.nextLine();
+        branch = sc.nextLine();
+        clg = sc.nextLine();
+        
+        System.out.println(name);
+        System.out.println(age);
+        System.out.println(branch);
+        System.out.println(clg);
+    }
+}
