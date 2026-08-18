cb4e1709a6a8d55e08cf1a8d8715e5623d4d4eef
Shiva kumar <24bk1a6675@stpetershyd.com>
2026-08-18 18:31:24 +0530

    Create Defination.java

---
+++ Basics/Programs/3)Inheritance/Defination.java
@@ -0,0 +1,3 @@
+Creating a new class based on exsisting class by adding new functionality (or) extending the class by adding new features without distrubibg the existing class and dependent application
+All the public and protected members od the exesting class can be accessed from derived class 
+All the public member of the existing class can be accessed using derived class object
