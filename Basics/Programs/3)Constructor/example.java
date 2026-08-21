eaad83b5848cfe8e13c456ffe120bda69e867c4c
Shiva kumar <24bk1a6675@stpetershyd.com>
2026-08-21 20:46:20 +0530

    Create example.java

---
+++ Basics/Programs/3)Constructor/example.java
@@ -0,0 +1,14 @@
+class parent
+  {
+    public parent()
+    {
+      System.out.print("constructor invoked");
+    }
+  }
+class program
+  {
+    public static void main(String args[])
+    {
+      parent p=new parent();
+    }
+  }
