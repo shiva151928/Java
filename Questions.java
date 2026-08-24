72695871060e13cf4cc1b1f4396517c897486a56
Shiva kumar <24bk1a6675@stpetershyd.com>
2026-08-24 14:41:51 +0530

    Create Questions.java

---
+++ Questions.java
@@ -0,0 +1,16 @@
+1)read 3 subject marks calculate total average and find grade on avarage 60,50,40,fail
+Arrays
+2) Find Element in an Array
+3) Addition of a Matrix
+4) Irregular arrays printing 1 to 50;
+Class
+5) Read 5 students print 5 student
+6) searching a student in an array
+Method OverLoading
+7)Write a List class for given program
+8) find(100) in it
+9) initailize array size from program 
+Constructors
+10) Initailize array size in the constructor
+Inheritance
+11) given a product and program write a list class
