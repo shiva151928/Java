96917c5d70f8c1f5c37747e80a7fa89f1d05fd21
Shiva kumar <24bk1a6675@stpetershyd.com>
2026-08-25 10:40:02 +0530

    Create Notes.java

---
+++ Concept/3)Constructor/Notes.java
@@ -0,0 +1,7 @@
+1) Constructor is also just like a method but it can automatically invoked when we create instance of a class
+2) Constructor name and class name must be same
+3) Constructor can take parameter but cant return a value
+4) Constructor should be public otherwise we cannot create instance 
+5) Constructor do not have return type
+6) If we wont write a contructor compiler will write a contructor by default
+7) Constructors are mainly used to initialize and instance data members of a class at the time of instance creation or object creation
