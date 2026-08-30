db051a88ea8b281de50e566f5476dc97af1178aa
Shiva kumar <24bk1a6675@stpetershyd.com>
2026-08-30 11:21:04 +0530

    Create inheritance4.java

---
+++ Concept/4)Inheritance/constructor/inheritance4.java
@@ -0,0 +1,29 @@
+class parent
+  {
+    public parent(int val)
+    {
+      System.out.print("parent constructor invoked"+val);
+    }
+    public parent()
+    {
+      System.out.print("parent constructor invoked");
+    }
+  }
+class child extends parent
+  {
+    public child(int val);
+    {
+      super(val);
+      System.out.print("child construcotr invoked"+val);
+    }
+  }
+class program
+  {
+    public static void main(String args[])
+    {
+      parent p;
+      child c;
+      p=new parent(100);
+      c=new child(200);
+    }
+  }
