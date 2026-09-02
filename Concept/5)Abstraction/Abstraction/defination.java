29438fcba9561472017793c165005a1317dbd2f8
Shiva kumar <24bk1a6675@stpetershyd.com>
2026-09-02 20:12:29 +0530

    Create defination.java

---
+++ Concept/5)Abstraction/Abstraction/defination.java
@@ -0,0 +1,3 @@
+Abstract is a class which has atleast one abstract method cant create instance of an abstract class can contain concrete methods 
+To use an abstract class we need to extend the class and override all abstract methods in the derived class otherwise derived also becomes abstract 
+Abstract method : is a method which has no body or defination is also called pure virtual function in c++
