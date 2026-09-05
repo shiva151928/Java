8191afd6aec0c49492f80275d7232c00d8eb81c9
Shiva kumar <24bk1a6675@stpetershyd.com>
2026-09-05 20:12:26 +0530

    Create defination.java

---
+++ Concept/6)Generics/defination.java
@@ -0,0 +1,8 @@
+1)Helps developer to create reusable identical software components which can work on user specified datatypes
+2)Generics are mainly used in collection classes like Stack | Queue | LinkedList
+3) Generic array are not possiable in java
+4)cant do arthemic operations on generics Type
+5)Java generic supports only classes cant use primitive type like Int|Double | char | float
+  All primitive types have class
+  Int -> Integer
+  Char -> Character
